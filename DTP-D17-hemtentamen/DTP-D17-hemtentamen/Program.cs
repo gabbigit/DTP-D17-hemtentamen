014c9b720615f41f313fcdc2504f73c15ebf5983
Gabriella <gabivrban@gmail.com>
2023-01-12 11:19:33 +0100

    'help

---
+++ DTP-D17-hemtentamen/DTP-D17-hemtentamen/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.Design;
+
+namespace DTP_D17_hemtentamen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the todo list, write 'help' for help!");
+            string command;
+            
+            do
+            {
+                Console.Write(">");
+                command = Console.ReadLine();
+                if(command == "help")
+                {
+                    Console.WriteLine("help - display this help text");
+                    Console.WriteLine("quit - quit the program");
+                }
+                else
+                {
+                    Console.WriteLine("Goodbye!");
+                }
+            }
+            while (command != "quit");   
+        }
+    }
+}
